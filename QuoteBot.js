7b56d50c131faade7dbcb92a0ce966b40421c10f
AlexHubble <alexanderhubble885@gmail.com>
2026-01-05 14:58:00 -0500

    Project architecture setup

--- QuoteBot.js
+++ QuoteBot.js
@@ -1,13 +1,38 @@
-//https://discordjs.guide/legacy - Guide followed for using Discord.js
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./Config.json');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-//Client Instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-//Runs once the client is ready
-client.once(Events.ClientReady, (readyClient) => {
-    console.log(`Client ready: ${readyClient.user.tag}`);
-});
+//Loads the command files from the folder
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandsFound = fs.readdirSync(foldersPath).filter((file) => file.endsWith('.js'));
+
+for (const file of commandsFound) {
+    const filePath = path.join(foldersPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+//Loads events from the events folder
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
 client.login(token);
