d2f406c4eedbc666ccfc0d75b1c5fe624db87937
AlexHubble <alexanderhubble885@gmail.com>
2026-01-12 09:20:58 -0500

    Fixed json duplication bug in QuoteStatFinder.js, fixed quotes being logged multiple times on re-run

--- Commands/QuoteStatFinder.js
+++ Commands/QuoteStatFinder.js
@@ -20,9 +20,7 @@ module.exports = {
         const outPutChannelOption = interaction.options.getChannel('outputchannel');
         const outPutChannel = await interaction.client.channels.fetch(outPutChannelOption.id)
         let quoteJson = require(path.join(dataPath, 'Quotes.json'));
-        const statsJson = require(path.join(dataPath, 'UserStats.json'));
-        let hasQuotedCounts = [];
-        let hasBeenQuotedCounts = [];
+        const statsJson = { users: {}}
 
         for(const key in quoteJson.quotes)
         {
@@ -65,36 +63,13 @@ module.exports = {
         }
 
         console.log("Gathered stats, pushing to json")
-        let userOutput = {"users": statsJson};
-
-        fs.writeFile(path.join(dataPath, 'UserStats.json'), JSON.stringify(userOutput, null, '\t'), 'utf8', (err) => {
+        fs.writeFile(path.join(dataPath, 'UserStats.json'), JSON.stringify(statsJson, null, '\t'), 'utf8', (err) => {
             if (err) {
                 console.error(err);
                 return;
             }
             console.log('Successfully written UserStats.json');
         });
-
-       /* hasBeenQuotedCounts.sort((a, b) => b.count - a.count)
-        hasQuotedCounts.sort((a, b) => b.count - a.count)
-
-        outPutChannel.send('**SPAMMING CHANNEL WITH QUOTE STATS PLEASE HOLD**')
-
-        outPutChannel.send('------------QUOTED COUNTS------------')
-        hasBeenQuotedCounts.forEach(count => {
-            outPutChannel.send(`User: **${count.name}** | Times user has been quoted: *${count.count}*`)
-        })
-
-        outPutChannel.send('------------HAS QUOTED COUNTS------------')
-        hasQuotedCounts.forEach(count => {
-            outPutChannel.send(`User: **${count.name}** | Times user has quoted others: *${count.count}*`)
-        })
-
-        outPutChannel.send('**ITS SAFE TO TALK AGAIN**')
-
-
-        console.log(hasBeenQuotedCounts);
-        console.log(hasQuotedCounts);*/
     },
 };
 
